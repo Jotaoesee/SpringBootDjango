21d0403515d63f605ba472d4fb2a8759fccf4f7f
Jotaoesee <jadiazm1995@gmail.com>
2025-02-20 11:30:03 +0100

    Añade modelos de Perfil y Producto en la API y aplica migraciones

--- backend_django/api/models.py
+++ backend_django/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Perfil(models.Model):
+    nombre = models.CharField(max_length=255)
+    edad = models.IntegerField()
+    tarjeta_credito = models.CharField(max_length=19)  # Formato: XXXX XXXX XXXX XXXX
+    caducidad = models.CharField(max_length=5)  # Formato: MM/YY
+    cvv = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre

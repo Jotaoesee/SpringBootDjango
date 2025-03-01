919bcccb482abdd0648832e3e535bb5323fad8fc
Jotaoesee <jadiazm1995@gmail.com>
2025-03-01 11:54:30 +0100

    configuro docker-compose

--- backend_spring/paypal-servicio/src/main/java/com/compuglobalhipermeganet/paypal_service/services/PayPalService.java
+++ backend_spring/paypal-servicio/src/main/java/com/compuglobalhipermeganet/paypal_service/services/PayPalService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PayPalService {
     private final String CLIENT_ID = "A1zV2yW3zFf8gJNB4-BNra7UsEWKVqFrc6ikihp0mUl5LUGyTDjVpogsYI7cmUwjp3p2kwjvx2aDzoMlw4";
-    private final String CLIENT_SECRET = "EHasvDq_465HxbF_yRf7qBpjvrjF1N_BMv_83djPw2nj4-bCMRDHRt2f9TBb-DxM5r1SxZG7K6dvVxqR";
+    private final String CLIENT_SECRET = "G8PjTr9u_qXhF_Y4Xs7zGvMk1RjeVr-83mLwOq2zZvk9-BFhVsTfQr5ZYk8JsDkLmY6BvX9J9j0w5xq";
 
     public String generarPago(double monto) {
         return "https://sandbox.paypal.com/checkout?paymentId=456";
